306c9d2b2705c9c6a194e2fda9f4336bf864fcec
karina heras <karinaheras@MacBook-Pro-de-karina.local>
2020-05-25 13:48:43 +0200

    terminado Login

--- src/app/login/private-posts/private-posts.component.ts
+++ src/app/login/private-posts/private-posts.component.ts
@@ -1,25 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { PostsService } from 'src/app/service/posts.service';
-
 @Component({
   selector: 'app-private-posts',
   templateUrl: './private-posts.component.html',
   styleUrls: ['./private-posts.component.css']
 })
 export class PrivatePostsComponent implements OnInit {
-posts = [];
-  constructor(private postsService: PostsService) { }
+
+  privatePosts = [];
+  constructor(private postsService: PostsService, private router: Router) { }
+
 
   ngOnInit(){
-    this.postsService.getPrivatePosts();
-      // .subscribe(
-      //   res => {
-      //     console.log(res);
-      //     this.posts = res;
-      //   },
-      //   err => console.log(err)
-      // );
-
+    this.postsService.getPrivatePosts()
+      .subscribe(
+        res => this.privatePosts = res,
+        err => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              this.router.navigate(['/signin']);
+            }
+          }
+        },
+      );
 
   }
 
